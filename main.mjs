b7f3a983fc37639b8e7cdae0f28f22c393abd1a1
dirac156 <dmurairimukongya@gmail.com>
2024-09-29 04:07:10 +0200

    1

---
+++ main.mjs
@@ -0,0 +1,8 @@
+// import loadChatbot from "immersiveai-chabot";
+// import loadChatbot from "immersiveai-chabot";
+const loadChatbot = require("immersiveai-chabot");
+
+loadChatbot(
+  "b7ab77f1-f0ea-4d4a-b1b5-926ed5105c42",
+  "11537177-389c-4a5c-b825-495dc9323bf5"
+);
